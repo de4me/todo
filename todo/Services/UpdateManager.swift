a8f15652afed5bd68cd23217e46eecdabab6290c
Denis Popov <info@de4me.com>
2026-03-11 04:00:16 +0300

    add updatemanager service

---
+++ todo/Services/UpdateManager.swift
@@ -0,0 +1,70 @@
+//
+//  UpdateManager.swift
+//  todo
+//
+//  Created by DE4ME on 09.03.2026.
+//
+
+import Foundation;
+import Network;
+
+
+protocol UpdateManagerProtocol: AnyObject {
+    func start();
+    func stop();
+}
+
+
+final class UpdateManager: UpdateManagerProtocol {
+    
+    static let shared: UpdateManagerProtocol = UpdateManager();
+    
+    private var monitor: NetworkMonitorInput!;
+    private var preferences: PreferencesProtocol!;
+    
+    init() {
+        self.preferences = PreferencesConfiguarator.configure();
+        self.monitor = NetworkMonitorConfigurator.configure(output: self);
+    }
+    
+    func start() {
+        self.monitor.start();
+    }
+    
+    func stop() {
+        self.monitor.stop();
+    }
+    
+}
+
+
+extension UpdateManager: NetworkMonitorOutput {
+    
+    private func databaseHandler(_ error: Error?) {
+        guard error == nil else {
+            return;
+        }
+        self.preferences.updated = true;
+    }
+    
+    private func updateHandler(_ result: Result<TodoResponce, Error>) {
+        switch result {
+        case .success(let responce):
+            Database.shared.save(todos: responce.todos, completionHandler: self.databaseHandler);
+        case .failure(let error):
+            print(error);
+            break;
+        }
+    }
+    
+    func networkMonitor(connected: Bool, interface: NWInterface.InterfaceType?) {
+        guard connected else {
+            return;
+        }
+        if !self.preferences.updated {
+            Server.shared.get(path: Server.Path.todos, type: TodoResponce.self, completionHandler: self.updateHandler);
+        }
+    }
+    
+}
+
