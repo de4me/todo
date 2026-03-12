83a56138032537ba188f33d5e23dba91afeba0a3
Denis Popov <info@de4me.com>
2026-03-12 13:50:55 +0300

    test Main module

---
+++ todoTests/testMainModule.swift
@@ -0,0 +1,40 @@
+//
+//  testMainModule.swift
+//  todoTests
+//
+//  Created by DE4ME on 12.03.2026.
+//
+
+import XCTest;
+@testable import todo;
+
+class MainViewDump: NSObject, MainConfiguratorProtocol & MainViewInput & UINavigationControllerDelegate {
+    var delegate: UINavigationControllerDelegate?;
+}
+
+
+final class testMainModule: XCTestCase {
+    
+    var view: MainViewDump?;
+    var presenter: MainPresenterProtocol?;
+
+    @MainActor
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.view = MainViewDump();
+        self.presenter = MainConfigurator.configure(view: self.view!);
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testConfigure() throws {
+        XCTAssertNotNil(self.view?.delegate);
+        XCTAssertNotNil(self.presenter?.view);
+        XCTAssertNotNil(self.presenter?.interactor);
+        XCTAssertNotNil(self.presenter?.interactor?.presenter);
+    }
+
+}
