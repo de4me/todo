3395a683bccc88f4e24192833041a81a1665804c
Denis Popov <info@de4me.com>
2026-03-13 14:53:52 +0300

    add label with attributes

---
+++ todo/Controls/LabelView.swift
@@ -0,0 +1,43 @@
+//
+//  LabelView.swift
+//  todo
+//
+//  Created by DE4ME on 13.03.2026.
+//
+
+import UIKit
+
+class LabelView: UILabel {
+
+    @IBInspectable var lineHeightMultiple: CGFloat = 1.2;
+    @IBInspectable var letterSpacing: CGFloat = 0;
+    
+    override var text: String! {
+        get {
+            super.text;
+        }
+        set {
+            self.update(string: newValue);
+        }
+    }
+    
+    private func update(string: String?) {
+        guard let string = string else {
+            self.attributedText = nil;
+            return;
+        }
+        let paragraph = NSMutableParagraphStyle();
+        paragraph.lineHeightMultiple = self.lineHeightMultiple;
+        paragraph.alignment = self.textAlignment;
+        let font = self.font ?? UIFont.systemFont(ofSize: UIFont.systemFontSize);
+        let textcolor = self.textColor ?? UIColor.label;
+        let attr: [NSAttributedString.Key:Any] = [
+            .font: font,
+            .foregroundColor: textcolor,
+            .paragraphStyle: paragraph,
+            .kern: self.letterSpacing
+        ]
+        self.attributedText = NSAttributedString(string: string, attributes: attr);
+    }
+
+}
